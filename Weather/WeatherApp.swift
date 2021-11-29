83b49c5c1e7f53bcf507982d0df4a22979b3de4c
Tino Tusaumwe <tusatino@gmail.com>
2021-11-29 20:40:27 +1000

    first commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tino on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
